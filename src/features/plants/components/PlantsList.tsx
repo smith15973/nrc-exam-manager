e1c669b12d6c4748b0b8090fa8f78acdaa25d276
Noah Smith <smith.15973@osu.edu>
2025-07-21 07:49:51 -0400

    changed plants list to use virutal component

--- src/features/plants/components/PlantsList.tsx
+++ src/features/plants/components/PlantsList.tsx
@@ -1,53 +1,68 @@
-// src/components/Test.tsx
-import { IconButton, List, ListItem, ListItemButton, ListItemAvatar, Avatar, SxProps } from '@mui/material';
+import { IconButton, ListItem, ListItemButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Link } from 'react-router-dom';
 import ConfirmDelete from '../../../common/components/ConfirmDelete';
-
+import ListWithSearch from '../../../common/components/ListWithSearch';
 
 
 interface PlantListProps {
   plants: Plant[];
   deletePlant: (plantId: number) => void;
-  sx?: SxProps
 }
 
-export default function PlantsList(props: PlantListProps) {
-  const { plants, deletePlant, sx } = props
-  return (
-    <List sx={sx}>
-      {plants.map((plant) => (
-        <ListItem
-          key={plant.plant_id}
-          secondaryAction={
-            <IconButton edge='end' aria-label='delete'>
-              <ConfirmDelete
-                message='Are you sure you want to delete this plant? This action cannot be undone!'
-                onConfirmDelete={() => deletePlant(plant.plant_id)}
-                button={({ onClick, disabled }) => (
-                  <DeleteIcon
-                    onClick={onClick}
-                    style={{
-                      cursor: disabled ? 'not-allowed' : 'pointer',
-                      opacity: disabled ? 0.5 : 1
-                    }}
-                  />
-                )}
-              />
+export default function PlantsList({ plants, deletePlant }: PlantListProps) {
 
-            </IconButton>
-          }
-          divider
-          disablePadding
+    // Render function for each item
+    const renderPlant = (plant: Plant, index: number, style: React.CSSProperties) => (
+        <ListItem
+            style={style}
+            key={plant.plant_id}
+            secondaryAction={
+                <IconButton edge='end' aria-label='delete'>
+                    <ConfirmDelete
+                        message='Are you sure you want to delete this Plant? This action cannot be undone!'
+                        onConfirmDelete={() => deletePlant(plant.plant_id)}
+                        button={({ onClick, disabled }) => (
+                            <DeleteIcon
+                                onClick={onClick}
+                                style={{
+                                    cursor: disabled ? 'not-allowed' : 'pointer',
+                                    opacity: disabled ? 0.5 : 1
+                                }}
+                            />
+                        )}
+                    />
+
+                </IconButton>
+            }
+            divider
+            disablePadding
         >
-          <ListItemAvatar>
-            <Avatar variant='rounded' alt={plant.name} />
-          </ListItemAvatar>
-          <ListItemButton component={Link} to={`/plants/${plant.plant_id}`}>
-            {plant.plant_id} {plant.name}
-          </ListItemButton>
+            <ListItemButton component={Link} to={`/plants/${plant.plant_id}`}>
+                {plant.plant_id} {plant.name}
+            </ListItemButton>
         </ListItem>
-      ))}
-    </List>
-  );
+    );
+
+    // Search filter function
+    const searchFilter = (plant: Plant, searchTerm: string) => {
+        return `${plant.plant_id} ${plant.name}`
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase());
+    };
+
+    // Key function for stable list items
+    const getItemKey = (plant: Plant) => plant.plant_id;
+
+    return (
+        <ListWithSearch
+            data={plants}
+            renderItem={renderPlant}
+            searchFilter={searchFilter}
+            searchPlaceholder="Search Plants..."
+            itemHeight={56}
+            noResultsMessage="No Plants found"
+            getItemKey={getItemKey}
+        />
+    );
 }
